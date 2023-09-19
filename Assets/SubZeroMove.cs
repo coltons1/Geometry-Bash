b6648ab78ccf94a5314bcb762479ea15fa57d291
Colton Stanek <colton.d.stanek@District158.org>
2023-09-19 08:48:50 -0500

    first script test

---
+++ Assets/SubZeroMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubZeroMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.name = "SubZeroDude";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
