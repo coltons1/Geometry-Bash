ea356c8e21ac63c0f7a0131020843d380b026bb8
Colton Stanek <colton.d.stanek@District158.org>
2023-11-02 09:07:17 -0500

    working on health bar canvas stuff

---
+++ Assets/Scripts/CanvasCamera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        //string sceneName = currentScene.name;
+
+        Canvas Healthbars;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(sceneName == "Stage1"){
+            
+        }*/
+    }
+}
--- Assets/Scripts/LoadSceneAssets.cs
+++ Assets/Scripts/LoadSceneAssets.cs
@@ -7,17 +7,19 @@ public class LoadSceneAssets : MonoBehaviour
     // Start is called before the first frame update
     private GameObject p1, p2;
     private GameObject h1, h2;
-    public int p1x, p1y, p2x, p2y;
-    public int h1x, h1y, h2x, h2y;
+    [SerializeField] int p1x, p1y, p2x, p2y;
+
     //sets p1 position to (p1x,p1y) and p2 position to (p2x,p2y)
     public void setPositions()
     {
         //assigns p1 and p2 to player 1 & player 2
         p1 = GameObject.Find("Player 1");
         p2 = GameObject.Find("Player 2");
+        
         //sets p1 position and velocity
         p1.transform.position = new Vector2(p1x, p1y);
         p1.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f,0.0f);
+        
         //sets p2 position and velocity
         p2.transform.position = new Vector2(p2x, p2y);
         p2.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f,0.0f);
@@ -25,6 +27,12 @@ public class LoadSceneAssets : MonoBehaviour
         //assigns h1 and h2 to player 1 health bar and player 2 healthbar
         h1 = GameObject.Find("p1Healthbar");
         h2 = GameObject.Find("p2Healthbar");
+
+        //h1.SetActive(true);
+        //h2.SetActive(true);
+
+        h1.transform.position = new Vector2(-560, 400);
+        h2.transform.position = new Vector2(600, 400);
 
 
 
