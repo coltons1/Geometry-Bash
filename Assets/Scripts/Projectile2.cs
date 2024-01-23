e9c84edbd79a8779f315f4181dc999276e17decd
Christian Rivera <christian.rivera@District158.org>
2024-01-23 08:50:25 -0600

    ranged attacks 81% done

--- Assets/Scripts/Projectile2.cs
+++ Assets/Scripts/Projectile2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public class Projectile2 : MonoBehaviour
 {
 public GameObject bullet;
 public Rigidbody2D bulletBody;
@@ -18,15 +18,15 @@ public LayerMask enemyLayer;
     // Update is called once per frame
     void Update()
     {
-       bulletBody.velocity = new Vector3(10f, 0f, 0f);
-        Collider2D[] hitEnemys = Physics2D.OverlapCircleAll(GameObject.Find("Projectile(Clone)").transform.position, 0.5f, enemyLayer);
+       bulletBody.velocity = new Vector3(-10f, 0f, 0f);
+        Collider2D[] hitEnemys = Physics2D.OverlapCircleAll(GameObject.Find("ProjectileTwo(Clone)").transform.position, 0.5f, enemyLayer);
         foreach(Collider2D enemy in hitEnemys){
             Debug.Log("hit");
-            enemy.GetComponent<Player2>().takeDamage(10);
-            Destroy(GameObject.Find("Projectile(Clone)"));
+            enemy.GetComponent<Player1>().takeDamage(10);
+            Destroy(GameObject.Find("ProjectileTwo(Clone)"));
         }
     }
     private void removeRangedAttack(){
-        Destroy(GameObject.Find("Projectile(Clone)"));
+        Destroy(GameObject.Find("ProjectileTwo(Clone)"));
     }
 }
