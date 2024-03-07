c11b965b6412e2ffb200d0c219a87c9a64aa37aa
Colton Stanek <colton.d.stanek@District158.org>
2024-03-07 08:39:38 -0600

    Back Button

--- Assets/Scripts/Apocalypse.cs
+++ Assets/Scripts/Apocalypse.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Apocalypse : MonoBehaviour
 {
+    GameObject p1, p2, h1, h2, h;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        p1 = GameObject.Find("Player 1");
+        p2 = GameObject.Find("Player 2");
+        h = GameObject.Find("Healthbars");
+        h1 = GameObject.Find("P1HealthBar");
+        h2 = GameObject.Find("P2HealthBar");
+    }
+
+    public void ApocalypseGo(){
+        Destroy(p1);
+        Destroy(p2);
+        Destroy(h1);
+        Destroy(h2);
+        Destroy(h);
     }
 
     // Update is called once per frame
