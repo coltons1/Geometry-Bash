62a730fb785d3e661773b98a25fcefef4757764b
Matthew Diaz <matthew.d.diaz@District158.org>
2024-03-12 08:43:28 -0500

    char select good for p1

--- Assets/Scripts/ihatecharacterselect.cs
+++ Assets/Scripts/ihatecharacterselect.cs
@@ -25,33 +25,30 @@ public class ihatecharacterselect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(this.transform.GetChild(0).gameObject.activeSelf == true){
-        //Debug.Log("Green active");
-            if(this == p1green){
-                p1blue.transform.GetChild(0).gameObject.SetActive(false);
-                Debug.Log("Green active");
-                p1yellow.transform.GetChild(0).gameObject.SetActive(false);
-                p1red.transform.GetChild(0).gameObject.SetActive(false);
-            }
-            else if(this == p1blue){
-                p1green.transform.GetChild(0).gameObject.SetActive(false);
-                p1yellow.transform.GetChild(0).gameObject.SetActive(false);
-                p1red.transform.GetChild(0).gameObject.SetActive(false);
-            }
-            else if(this == p1yellow){
-                p1blue.transform.GetChild(0).gameObject.SetActive(false);
-                p1green.transform.GetChild(0).gameObject.SetActive(false);
-                p1red.transform.GetChild(0).gameObject.SetActive(false);
-            }
-            else if(this == p1red){
-                p1blue.transform.GetChild(0).gameObject.SetActive(false);
-                p1yellow.transform.GetChild(0).gameObject.SetActive(false);
-                p1green.transform.GetChild(0).gameObject.SetActive(false);
-            }
-        }
+
     }
 
     public void OnClick(){
         this.transform.GetChild(0).gameObject.SetActive(true);
+        if(this.gameObject == p1green){
+            p1blue.transform.GetChild(0).gameObject.SetActive(false);
+            p1yellow.transform.GetChild(0).gameObject.SetActive(false);                
+            p1red.transform.GetChild(0).gameObject.SetActive(false);
+        }
+        else if(this.gameObject == p1blue){
+            p1green.transform.GetChild(0).gameObject.SetActive(false);
+            p1yellow.transform.GetChild(0).gameObject.SetActive(false);
+            p1red.transform.GetChild(0).gameObject.SetActive(false);
+        }
+        else if(this.gameObject == p1yellow){
+            p1blue.transform.GetChild(0).gameObject.SetActive(false);
+            p1green.transform.GetChild(0).gameObject.SetActive(false);
+            p1red.transform.GetChild(0).gameObject.SetActive(false);
+        }
+        else if(this.gameObject == p1red){
+            p1blue.transform.GetChild(0).gameObject.SetActive(false);
+            p1yellow.transform.GetChild(0).gameObject.SetActive(false);
+            p1green.transform.GetChild(0).gameObject.SetActive(false);
+        }
     }
 }
