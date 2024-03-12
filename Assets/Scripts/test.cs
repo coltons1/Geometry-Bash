ef58da506d502010d78303354bfeef4d1b73d865
Christian Rivera <christian.rivera@District158.org>
2024-03-12 08:46:03 -0500

    pulling contnent

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -3,40 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ihatecharacterselect : MonoBehaviour
+public class test : MonoBehaviour
 {
     [SerializeField] private Image image;
     [SerializeField] private Color color;
-    private GameObject charSelect;
+    private GameObject p1Blue;
+    private GameObject p1Green;
+
     public bool isActive = false;
 
     // Start is called before the first frame update
     void Start()
     {
         p1Green = GameObject.Find("GreenCharSelect");
-        p1Blue = GameObject.FInd("BlueCharSelect");
-        p1Red = 
+        p1Blue = GameObject.Find("BlueCharSelect");
+        //p1Red = 
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(this.transform.GetChild(0).gameObject.activeSelf() == true){
+        if(this.transform.GetChild(0).gameObject.activeSelf == true){
             if(this == p1Green){
                 p1Blue.transform.GetChild(0).gameObject.SetActive(false);
-                p1Red.transform.GetChild(0).gameObject.SetActive(false);
+               // p1Red.transform.GetChild(0).gameObject.SetActive(false);
             }
             else if(this == p1Blue){
                 p1Green.transform.GetChild(0).gameObject.SetActive(false);
-                p1Red.transform.GetChild(0).gameObject.SetActive(false);
+                //p1Red.transform.GetChild(0).gameObject.SetActive(false);
 
             }
-            else if(this == p1Red){
-                p1Green.transform.GetChild(0).gameObject.SetActive(false);
-                p1Green.transform.GetChild(0).gameObject.SetActive(false);
+           // else if(this == p1Red){
+            //    p1Green.transform.GetChild(0).gameObject.SetActive(false);
+                //p1Green.transform.GetChild(0).gameObject.SetActive(false);
 
-            }
+           // }
         }
 
         
