bb12bcb4daa514822426fb6bd2177dacf39d2e81
Matthew Diaz <matthew.d.diaz@District158.org>
2024-03-12 09:03:26 -0500

    100 pages by 5th hour... devastating

--- Assets/Scripts/ihatecharacterselect.cs
+++ Assets/Scripts/ihatecharacterselect.cs
@@ -11,6 +11,11 @@ public class ihatecharacterselect : MonoBehaviour
     public GameObject p1blue;
     public GameObject p1yellow;
     public GameObject p1red;
+    public GameObject p2green;
+    public GameObject p2blue;
+    public GameObject p2yellow;
+    public GameObject p2red;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +24,11 @@ public class ihatecharacterselect : MonoBehaviour
         p1blue = GameObject.Find("BlueCharSelect");   
         p1yellow = GameObject.Find("YellowCharSelect");
         p1red = GameObject.Find("RedCharSelect");
+
+        p2green = GameObject.Find("GreenCharSelect player 2");
+        p2blue = GameObject.Find("BlueCharSelect player 2");   
+        p2yellow = GameObject.Find("YellowCharSelectplayer 2");
+        p2red = GameObject.Find("RedCharSelect player 2");
 
     }
 
@@ -49,6 +59,28 @@ public class ihatecharacterselect : MonoBehaviour
             p1blue.transform.GetChild(0).gameObject.SetActive(false);
             p1yellow.transform.GetChild(0).gameObject.SetActive(false);
             p1green.transform.GetChild(0).gameObject.SetActive(false);
+        }
+
+        //player 2 boxes
+        if(this.gameObject == p2green){
+            p2blue.transform.GetChild(0).gameObject.SetActive(false);
+            p2yellow.transform.GetChild(0).gameObject.SetActive(false);                
+            p2red.transform.GetChild(0).gameObject.SetActive(false);
+        }
+        else if(this.gameObject == p2blue){
+            p2green.transform.GetChild(0).gameObject.SetActive(false);
+            p2yellow.transform.GetChild(0).gameObject.SetActive(false);
+            p2red.transform.GetChild(0).gameObject.SetActive(false);
+        }
+        else if(this.gameObject == p2yellow){
+            p2blue.transform.GetChild(0).gameObject.SetActive(false);
+            p2green.transform.GetChild(0).gameObject.SetActive(false);
+            p2red.transform.GetChild(0).gameObject.SetActive(false);
+        }
+        else if(this.gameObject == p2red){
+            p2blue.transform.GetChild(0).gameObject.SetActive(false);
+            p2yellow.transform.GetChild(0).gameObject.SetActive(false);
+            p2green.transform.GetChild(0).gameObject.SetActive(false);
         }
     }
 }
